00d85c57a54c1c7a02dee1d051b991cfb873e0a4
李晓佳 <353243186@qq.com>
2019-05-21 17:16:06 +0800

    公众号完成

--- Source/Synchronization/Transcoders/ZMConversationTranscoder.m
+++ Source/Synchronization/Transcoders/ZMConversationTranscoder.m
@@ -429,6 +429,7 @@ static NSString *const ConversationTeamManagedKey = @"managed";
         [conversation updateServerModified:serverTimestamp];
         NSUUID * const userId = [payloadData uuidForKey:@"from"];
         ZMUser *user = [ZMUser userWithRemoteID:userId createIfNeeded:YES inContext:self.managedObjectContext];
+        user.needsToBeUpdatedFromBackend = true;
         [conversation internalAddParticipants:[NSSet setWithObject:user]];
         [self.managedObjectContext enqueueDelayedSave];
         user.connection.conversation = conversation;
