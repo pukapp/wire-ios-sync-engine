552ad536f3bc8a34584259f1b3e9f773bef1ae97
wangjie <17054214@qq.com>
2020-09-21 17:38:01 +0800

    静音会话通知bug修复

--- Source/Notifications/Push notifications/Notification Types/Content/ZMLocalNotification+NSEMessages.swift
+++ Source/Notifications/Push notifications/Notification Types/Content/ZMLocalNotification+NSEMessages.swift
@@ -11,10 +11,30 @@ import Foundation
 public class NSEMessageNotificationBuilder: MessageNotificationBuilder  {
     
     override func shouldCreateNotification() -> Bool {
-        guard !message.isSilenced else {
-            return false
+        let silenced = { [weak self] () -> Bool  in
+            
+            guard let self = self else {return true}
+            
+            guard let sender = self.message.sender, !sender.isSelfUser else {
+                return true
+            }
+            
+            if self.conversation.mutedMessageTypesIncludingAvailability == .none {
+                return false
+            }
+            
+            guard let textMessageData = self.message.textMessageData else {
+                return true
+            }
+            
+            if self.conversation.mutedMessageTypesIncludingAvailability == .regular && (textMessageData.isMentioningSelf || textMessageData.isQuotingSelf) {
+                return false
+            } else {
+               return true
+            }
         }
-        return true;
+        
+        return !silenced()
     }
     
 }
