e5784c6c7e01c1f77ebd691ca06383e0769f10cc
chinwy <amberchin@163.com>
2020-09-26 11:58:30 +0800

    新增一种通知监控刷新任务详情

--- Source/Synchronization/Transcoders/ZMConversationTranscoder.m
+++ Source/Synchronization/Transcoders/ZMConversationTranscoder.m
@@ -463,8 +463,9 @@ NSNotificationName const ZMUpdateTaskListNotificationName = @"ZMUpdateTaskListNo
             event.type == ZMUpdateEventTypeITaskDynamicMessageAdd){
             NSDictionary *dictionaryInfo = [event.payload dictionaryForKey:@"data"];
             NSString *msgType = [dictionaryInfo valueForKey:@"msgType"];
-            if ([msgType  isEqual: @"30001"] || [msgType  isEqual: @"30006"] || [msgType  isEqual: @"30007"]) {
-                //30001 : 加入项目   30006 : 加入任务关注者  30007 : 加入任务执行者
+            if ([msgType  isEqual: @"30001"] || [msgType  isEqual: @"30006"] || [msgType  isEqual: @"30007"] || [msgType  isEqual: @"40007"]) {
+                NSLog(@"msgType : %@",msgType);
+                //30001 : 加入项目   30006 : 加入任务关注者  30007 : 加入任务执行者 40007 : 加入任务执行者
                 [[NSNotificationCenter defaultCenter] postNotificationName:ZMUpdateTaskListNotificationName object:nil userInfo:nil];
             }
         }
